cab5e7d539d22142e68e763cc1c14c4d4ac34131
HokageM <m.trzaska663@gmail.com>
2024-03-28 18:33:05 +0100

    update description

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(description="The ByakuganVisualizer repository hosts a Python tool designed to compare images and "
+                          "highlight their differences. It simplifies the process of identifying disparities between "
+                          "images, making it ideal for tasks like testing and quality assurance. Additionally, it "
+                          "offers options for customization, which can be helpful for color-blind users.")
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
